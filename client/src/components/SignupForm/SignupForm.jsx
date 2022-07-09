fdb61aa1863c62564525e68f98220f2335be2054
Laura Walters <laura.walters1098@yahoo.com>
2022-07-09 19:28:19 +0100

    handle user input

--- client/src/components/SignupForm/SignupForm.jsx
+++ client/src/components/SignupForm/SignupForm.jsx
@@ -1,5 +1,7 @@
 // import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 
 import Auth from '../../utils/auth';
 
@@ -7,12 +9,52 @@ import Auth from '../../utils/auth';
 import { ADD_USER } from '../../utils/mutations';
 
 const SignupForm = () => {
+    // set initial form state
+    const [userFormData, setUserFormData] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+  
+    const [addUser, { error }] = useMutation(ADD_USER);
+  
+    const handleUserInput = (event) => {
+      const { name, value } = event.target;
+      setUserFormData({ ...userFormData, [name]: value });
+    };
+  
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+      // check if form has everything (as per react-bootstrap docs)
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+  
+      try {
+        const { data } = await addUser({
+          variables: { ...userFormData },
+        });
+        Auth.login(data.addUser.token);
+      } catch (err) {
+        alert(err);
+      }
+  
+      setUserFormData({
+        username: '',
+        email: '',
+        password: '',
+      });
+    };
   return (
+
     <>
-      <form action="">
-      <input type="text" placeholder='email' />
-      <input type="text" placeholder='username' />
-      <input type="text" placeholder='password' />
+      <form action="" onSubmit={handleFormSubmit}>
+      <input type="text" placeholder='email' name='email' onChange={handleUserInput}/>
+      <input type="text" placeholder='username' name='username' onChange={handleUserInput} />
+      <input type="password" placeholder='password' name='password' onChange={handleUserInput} />
       <button>Submit</button>
       </form>
     </>

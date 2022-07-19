e6eb3e009966e234ab7e587cd2b8e9a9957f31fd
Laura Walters <laura.walters1098@yahoo.com>
2022-07-19 17:37:03 +0100

    user login working

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,13 +1,52 @@
 // import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+
+// mutations/queries
+import { LOGIN_USER } from '../../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
+
+import Auth from '../../utils/auth';
 
 const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+  const handleUserInput = (event) => {
+    const { name, value } = event.target;
+      setUserFormData({ ...userFormData, [name]: value });
+  }
+
+  const handleFormSubmit = async (event)=>{
+    event.preventDefault()
+
+    try {
+      console.log(userFormData)
+      const { data } = await loginUser({
+        variables:  {...userFormData },
+      });
+      console.log("done")
+      Auth.login(data.loginUser.token);
+     
+    } catch (err) {
+      alert(err);
+    }
+
+    setUserFormData({
+      email: '',
+      password: '',
+    });
+  }
+
   return (
     <>
 
-      <form action="">
-      <input type="text" placeholder='email' />
-      <input type="text" placeholder='password' />
+      <form action="" onSubmit={handleFormSubmit}>
+      <input type="text" placeholder='email' name='email' onChange={handleUserInput}/>
+      <input type="text" placeholder='password' name='password' onChange={handleUserInput} />
       <button>Submit</button>
       </form>
     </>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,3 +14,14 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        username
+      }
+    }
+  }
+`;
